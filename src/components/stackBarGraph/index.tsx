f909fba6c5df278a00f8c0b712f8f2a76c0a7385
Mohit Kumar <mohitk@geekyants.com>
2019-10-29 19:56:54 +0530

    feat(stackBarGraph.tsx):stackBarGraph component added

--- src/components/stackBarGraph/index.tsx
+++ src/components/stackBarGraph/index.tsx
@@ -1,95 +1,228 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, Alert} from 'react-native';
 import {StackedBarChart} from 'react-native-svg-charts';
 
+const GRAPH_OFFSET = 6;
+const HIT_SLOP = {left: 10, right: 10, top: 10, bottom: 10};
+const GRAPH_HEIGHT = 200;
+let minStackHeight = 0;
+let monthlyTarget = 300;
+let emi = 200;
 interface props {}
 
-interface state {}
+interface state {
+  currentScrollIndex: number;
+}
 
-var monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
+/**
+ * Sample test data used for graph plotting
+ */
+
+let monthNames = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
 let currentMonth = 'May';
 const data = [
   {
     month: new Date(2015, 0, 1),
-    emi: 200,
-    monthlyTarget: 300,
+    emi: emi,
+    overPayment: 300,
   },
   {
     month: new Date(2015, 1, 1),
-    emi: 200,
-    monthlyTarget: 370,
+    emi: emi,
+    overPayment: 370,
   },
   {
     month: new Date(2015, 2, 1),
-    emi: 200,
-    monthlyTarget: 270,
+    emi: emi,
+    overPayment: 270,
   },
   {
     month: new Date(2015, 3, 1),
-    emi: 200,
-    monthlyTarget: 390,
+    emi: emi,
+    overPayment: 350,
   },
   {
     month: new Date(2015, 0, 1),
-    emi: 200,
-    monthlyTarget: 400,
+    emi: emi,
+    overPayment: 100,
   },
   {
     month: new Date(2015, 1, 1),
-    emi: 200,
-    monthlyTarget: 210,
+    emi: emi,
+    overPayment: 210,
+  },
+  {
+    month: new Date(2015, 0, 1),
+    emi: emi,
+    overPayment: 350,
+  },
+  {
+    month: new Date(2015, 1, 1),
+    emi: emi,
+    overPayment: 270,
+  },
+  {
+    month: new Date(2015, 2, 1),
+    emi: emi,
+    overPayment: 370,
+  },
+  {
+    month: new Date(2015, 3, 1),
+    emi: emi,
+    overPayment: 0,
+  },
+  {
+    month: new Date(2015, 0, 1),
+    emi: emi,
+    overPayment: 200,
+  },
+  {
+    month: new Date(2015, 1, 1),
+    emi: emi,
+    overPayment: 310,
   },
 ];
 
-const colors = ['#989999', '#BEC0C0'];
-const keys = ['emi', 'monthlyTarget'];
+const COLORS = ['#989999', '#BEC0C0'];
+const KEYS = ['emi', 'overPayment'];
 
 export class StackBarGraph extends React.Component<props, state> {
   constructor(props: any) {
     super(props);
-    this.state = {};
+    this.state = {
+      currentScrollIndex: 0,
+    };
   }
+
+  handleGraphLeftSwipe = () => {
+    if (this.state.currentScrollIndex)
+      this.setState({
+        currentScrollIndex: this.state.currentScrollIndex - 1,
+      });
+  };
+
+  handleGraphRightSwipe = () => {
+    if (this.state.currentScrollIndex < monthNames.length / GRAPH_OFFSET - 1)
+      this.setState({
+        currentScrollIndex: this.state.currentScrollIndex + 1,
+      });
+  };
+
   render() {
+    let currData = data.slice(
+      this.state.currentScrollIndex * GRAPH_OFFSET,
+      GRAPH_OFFSET * (this.state.currentScrollIndex + 1),
+    );
+    let currMonthName = monthNames.slice(
+      this.state.currentScrollIndex * GRAPH_OFFSET,
+      GRAPH_OFFSET * (this.state.currentScrollIndex + 1),
+    );
     return (
       <View>
-        <Text style={styles.dottedText}>
-          ------------------------------------------------------------
-        </Text>
-        <StackedBarChart
-          style={styles.barChart}
-          keys={keys}
-          colors={colors}
-          data={data}
-          showGrid={false}
-          contentInset={{top: 30, left: 30, right: 30}}
-          animate={true}
-          spacingInner={0.3}
-        />
-        <View>
-          <View style={styles.monthView}>
-            {monthNames.map((item, index) => {
-              return (
-                <Text
+        <View style={styles.mainContainer}>
+          <TouchableOpacity
+            onPress={() => this.handleGraphLeftSwipe()}
+            style={{alignSelf: 'center'}}
+            hitSlop={HIT_SLOP}>
+            <Text style={{fontSize: 32, alignSelf: 'center'}}>{'<'}</Text>
+          </TouchableOpacity>
+          <View style={{flex: 1, marginHorizontal: 4}}>
+            <TouchableOpacity
+              onPress={() => Alert.alert('Navigate to Monthly details')}>
+              <View>
+                <View
                   style={{
-                    fontSize: 20,
-                    color: currentMonth === item ? '#7C7C7C' : '#989999',
-                    fontStyle: 'italic',
-                    textDecorationLine: currentMonth === item && 'underline',
+                    position: 'absolute',
+                    height:
+                      ((GRAPH_HEIGHT - 24) / monthlyTarget) *
+                      (GRAPH_HEIGHT - 24),
+                    bottom: 0,
+                    flex: 1,
+                    width: '100%',
+                    borderTopColor: '#7C7C7C',
+                    borderTopWidth: 0.3,
+                    zIndex: 1,
                   }}
-                  key={index}>
-                  {item}
-                </Text>
-              );
-            })}
-          </View>
-          <View style={{flexDirection: 'row'}}>
-            <View style={styles.monthDetails}>
-              <Text style={styles.monthDetailsText}>3 Months</Text>
+                />
+                <StackedBarChart
+                  style={styles.barChart}
+                  keys={KEYS}
+                  colors={COLORS}
+                  data={currData}
+                  showGrid={false}
+                  contentInset={{
+                    top: 24,
+                    left: 24,
+                    right: 24,
+                  }}
+                  animate={true}
+                  spacingInner={0.3}
+                />
+              </View>
+              <View style={styles.monthView}>
+                {currMonthName.map((item, index) => {
+                  return (
+                    <Text
+                      style={{
+                        fontSize: 20,
+                        color: currentMonth === item ? '#7C7C7C' : '#989999',
+                        fontStyle: 'italic',
+                        textDecorationLine:
+                          currentMonth === item && 'underline',
+                      }}
+                      key={index}>
+                      {item}
+                    </Text>
+                  );
+                })}
+              </View>
+            </TouchableOpacity>
+            <View>
+              <View style={{flexDirection: 'row'}}>
+                <View
+                  style={[
+                    styles.monthDetails,
+                    {borderRightColor: '#989999', borderRightWidth: 1},
+                  ]}>
+                  <Text style={styles.monthHeaderText}>
+                    Projected Time Saving
+                  </Text>
+                  <Text style={styles.monthDetailsText}>3 Months</Text>
+                </View>
+                <View style={styles.monthDetails}>
+                  <Text style={styles.monthHeaderText}>
+                    Projected Interest Saving
+                  </Text>
+                  <Text
+                    style={[
+                      styles.monthDetailsText,
+                      {width: '100%', marginLeft: 4},
+                    ]}>
+                    £ 175
+                  </Text>
+                </View>
+              </View>
             </View>
-            <View style={styles.monthDetails}>
-              <Text style={{color: '#7C7C7C', fontSize: 24}}>175</Text>
-            </View>
           </View>
+          <TouchableOpacity
+            onPress={() => this.handleGraphRightSwipe()}
+            style={{alignSelf: 'center'}}
+            hitSlop={HIT_SLOP}>
+            <Text style={{fontSize: 32}}>{'>'}</Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
@@ -97,13 +230,13 @@ export class StackBarGraph extends React.Component<props, state> {
 }
 
 const styles = StyleSheet.create({
-  dottedText: {
-    position: 'absolute',
-    bottom: 218,
-    color: '#7C7C7C',
-    zIndex: 1,
+  mainContainer: {flexDirection: 'row'},
+  barChart: {
+    height: GRAPH_HEIGHT,
+    backgroundColor: '#DEDFDF',
+    borderBottomColor: '#BEC0C0',
+    borderBottomWidth: 1,
   },
-  barChart: {height: 250, backgroundColor: '#DEDFDF'},
   monthView: {
     flexDirection: 'row',
     padding: 32,
@@ -118,4 +251,10 @@ const styles = StyleSheet.create({
     width: '50%',
   },
   monthDetailsText: {color: '#7C7C7C', fontSize: 24},
+  monthHeaderText: {
+    fontSize: 14,
+    color: '#908F8F',
+    fontStyle: 'italic',
+    paddingBottom: 4,
+  },
 });
