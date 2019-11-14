c66bf30c367bdb2053953c358e01ccfb4ffdf855
Mohit Kumar <mohitk@geekyants.com>
2019-11-14 17:46:26 +0530

    feat(Dashboard) : UI and styles added

--- src/screens/dashBoard/index.tsx
+++ src/screens/dashBoard/index.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  ScrollView,
+  ImageBackground,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
 import {Button} from 'react-native-elements';
 import {styles} from './styles';
-import {Header} from '../../components';
+import {dashBoardCard, report, plusIncome, minusIncome} from '../../assets';
 import {connect} from 'react-redux';
+import {StackBarGraph} from '../../components';
+import * as Progress from 'react-native-progress';
 import {get as _get} from 'lodash';
+import {localeString} from '../../utils/i18n';
 import {setAuthToken, showSnackBar} from '../../utils/helperFunctions';
 import {
   NAVIGATION_SCREEN_NAME,
   DB_KEYS,
   APP_CONSTANTS,
+  LOCALE_STRING,
 } from '../../utils/constants';
 
 interface props {
@@ -47,17 +58,111 @@ export class UnconnectedDashBoard extends React.Component<props, state> {
   render() {
     return (
       <View style={styles.mainContainer}>
-        <Header />
-        <View style={styles.middleContainer}>
-          <Text>DashBaord Screen</Text>
-        </View>
-        <Button
-          title="Log Out"
-          onPress={() => {
-            this.setState({isLoggedOut: true});
-            this.handleLogOut();
-          }}
-        />
+        <ScrollView
+          contentContainerStyle={styles.middleContainer}
+          showsVerticalScrollIndicator={false}>
+          <ImageBackground
+            source={dashBoardCard}
+            style={styles.blueImageBackground}>
+            <TouchableOpacity
+              onPress={() => {
+                /*
+                TODO : Temporarily Logout on this button, have to be moved
+                */
+                this.setState({isLoggedOut: true});
+                this.handleLogOut();
+              }}
+              style={styles.supportIcon}>
+              <Image source={report} />
+            </TouchableOpacity>
+            <Text style={styles.thisMonthText}>
+              {localeString(LOCALE_STRING.DASHBOARD_SCREEN.THIS_MONTH)}
+            </Text>
+            <Text style={styles.overPaymentTargetAmount}>£175</Text>
+            <Text style={styles.overPaymentTargetText}>
+              {localeString(LOCALE_STRING.DASHBOARD_SCREEN.OVER_PAYMENT)}
+            </Text>
+            <Text style={styles.dueReminderText}>
+              {localeString(LOCALE_STRING.DASHBOARD_SCREEN.PAYMENT_REMINDER)}
+            </Text>
+          </ImageBackground>
+          <View style={styles.secondContainer}>
+            <View style={styles.statusContainer}>
+              <Text style={styles.statusLefttext}>
+                {localeString(LOCALE_STRING.DASHBOARD_SCREEN.AMOUNT_LEFT)}{' '}
+                <Text style={styles.innerFirstText}>£ 2880</Text>
+              </Text>
+              <Text style={styles.statusRightText}>
+                {/* /*
+              TODO : Will be moved to locales after data model
+              */}
+                Spent £ 560 out of £ 3440
+              </Text>
+            </View>
+            <View style={styles.passStrengthInnerContainer}>
+              <Progress.Bar
+                progress={0.6}
+                color="#FF9D00"
+                height={10}
+                width={null}
+                borderRadius={5}
+                unfilledColor="#FFE9B3"
+                borderWidth={0}
+              />
+            </View>
+          </View>
+          {/* Shadow effect is to be enhanced further */}
+          <View style={styles.incomeStatusContainer}>
+            <View style={styles.incomeInnerContainer}>
+              <View style={styles.rowParentContainer}>
+                <View style={styles.incomeInnerContainers}>
+                  <View style={styles.rowParentContainer}>
+                    <Image source={plusIncome} height={18} width={18} />
+                    <Text style={styles.incomeText}>
+                      {localeString(LOCALE_STRING.DASHBOARD_SCREEN.INCOME)}
+                    </Text>
+                  </View>
+                  <Text style={styles.incomeSpentText}>£3,440.00</Text>
+                </View>
+                <View style={styles.incomeInnerContainers}>
+                  <View style={styles.rowParentContainer}>
+                    <Image source={minusIncome} height={18} width={18} />
+                    <Text style={styles.spentText}>
+                      {localeString(LOCALE_STRING.DASHBOARD_SCREEN.SPENT)}
+                    </Text>
+                  </View>
+                  <Text style={styles.incomeSpentText}>£3,440.00</Text>
+                </View>
+              </View>
+              <Text style={styles.availableBalanceText}>
+                {localeString(LOCALE_STRING.DASHBOARD_SCREEN.AVAILABLE_BALANCE)}
+              </Text>
+              <Text style={styles.availableAmountText}>£ 21,312.00</Text>
+              <Button
+                title={localeString(
+                  LOCALE_STRING.DASHBOARD_SCREEN.MAKE_OVERPAYMENT,
+                )}
+                titleStyle={styles.buttonTitle}
+                buttonStyle={styles.button}
+              />
+            </View>
+          </View>
+          {/* View for graph */}
+          <View>
+            <View style={styles.myProgressContainer}>
+              <Text style={styles.myProgressText}>
+                {localeString(LOCALE_STRING.DASHBOARD_SCREEN.MY_PROGRESS)}
+              </Text>
+              <Text style={styles.projectedText}>
+                {localeString(
+                  LOCALE_STRING.DASHBOARD_SCREEN.PROJECTED_MORTGAGE,
+                )}{' '}
+                <Text style={styles.monthsLeftText}>3yr 11m</Text>
+              </Text>
+            </View>
+            <StackBarGraph />
+          </View>
+        </ScrollView>
       </View>
     );
   }
--- src/screens/dashBoard/styles.tsx
+++ src/screens/dashBoard/styles.tsx
@@ -1,13 +1,144 @@
 import {StyleSheet} from 'react-native';
+import {STYLE_CONSTANTS} from '../../utils/constants';
+import {COLOR} from '../../utils/colors';
 
 export const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
   },
-  middleContainer: {
-    flex: 1,
-    marginHorizontal: 16,
-    justifyContent: 'center',
-    alignItems: 'center',
+  middleContainer: {},
+  passStrengthInnerContainer: {
+    marginTop: STYLE_CONSTANTS.margin.SMALLER,
   },
+  blueImageBackground: {
+    paddingLeft: STYLE_CONSTANTS.padding.HUGISH,
+    paddingTop: STYLE_CONSTANTS.padding.SMALL,
+    paddingBottom: STYLE_CONSTANTS.padding.HUMONGOUS,
+    paddingRight: STYLE_CONSTANTS.padding.LARGEST,
+  },
+  supportIcon: {
+    height: STYLE_CONSTANTS.padding.HUMONGOUS,
+    width: STYLE_CONSTANTS.padding.HUMONGOUS,
+    borderRadius: STYLE_CONSTANTS.padding.LARGEST,
+    padding: STYLE_CONSTANTS.padding.SMALLER,
+    backgroundColor: COLOR.DARK_VOILET,
+    alignSelf: 'flex-end',
+  },
+  thisMonthText: {
+    marginTop: STYLE_CONSTANTS.margin.SMALL,
+    fontSize: STYLE_CONSTANTS.font.SIZE.HUGE,
+    lineHeight: STYLE_CONSTANTS.font.LINEHEIGHT.HUGEST,
+    color: COLOR.WHITE,
+    fontWeight: STYLE_CONSTANTS.font.WEIGHT.SEMI_BOLD,
+  },
+  overPaymentTargetAmount: {
+    marginTop: STYLE_CONSTANTS.margin.LARGEST,
+    fontSize: STYLE_CONSTANTS.font.SIZE.HUMONGOUS,
+    lineHeight: STYLE_CONSTANTS.font.LINEHEIGHT.LARGEST_HUMONGOUS,
+    color: COLOR.WHITE,
+    fontWeight: STYLE_CONSTANTS.font.WEIGHT.SEMI_BOLD,
+  },
+  overPaymentTargetText: {
+    fontSize: STYLE_CONSTANTS.font.SIZE.SMALL,
+    lineHeight: STYLE_CONSTANTS.font.LINEHEIGHT.HUGE,
+    color: COLOR.WHITE,
+    opacity: 0.5,
+  },
+  dueReminderText: {
+    fontSize: STYLE_CONSTANTS.font.SIZE.NORMAL,
+    lineHeight: STYLE_CONSTANTS.font.LINEHEIGHT.HUGISH,
+    color: COLOR.WHITE,
+    marginTop: STYLE_CONSTANTS.margin.LARGER,
+  },
+  secondContainer: {
+    marginTop: STYLE_CONSTANTS.margin.LARGEST,
+    marginHorizontal: STYLE_CONSTANTS.margin.LARGEST,
+  },
+  statusContainer: {justifyContent: 'space-between', flexDirection: 'row'},
+  statusLefttext: {
+    color: COLOR.LIGHT_VOILET,
+    fontWeight: STYLE_CONSTANTS.font.WEIGHT.SEMI_BOLD,
+  },
+  innerFirstText: {color: COLOR.VOILET},
+  statusRightText: {color: COLOR.VOILET, opacity: 0.5},
+  incomeStatusContainer: {
+    borderBottomColor: '#0000001A',
+    borderColor: '#fff',
+    borderBottomWidth: 5,
+    borderWidth: 1,
+    borderRadius: 20,
+    shadowColor: '#0000001A',
+    shadowOffset: {width: 3, height: 3},
+    shadowOpacity: 10,
+    shadowRadius: 10,
+  },
+  incomeInnerContainer: {
+    marginTop: STYLE_CONSTANTS.margin.HUMONGOUS,
+    marginHorizontal: STYLE_CONSTANTS.margin.HUGISH,
+  },
+  rowParentContainer: {flexDirection: 'row'},
+  incomeInnerContainers: {flex: 1, alignItems: 'center'},
+  incomeText: {
+    marginLeft: STYLE_CONSTANTS.margin.SMALLER,
+    color: COLOR.PARROT_GREEN,
+    fontSize: STYLE_CONSTANTS.font.SIZE.SMALL,
+    lineHeight: STYLE_CONSTANTS.font.LINEHEIGHT.HUGISH,
+  },
+  spentText: {
+    marginLeft: STYLE_CONSTANTS.margin.SMALLER,
+    color: COLOR.BRICK_RED,
+    fontSize: STYLE_CONSTANTS.font.SIZE.SMALL,
+    lineHeight: STYLE_CONSTANTS.font.LINEHEIGHT.HUGISH,
+  },
+  incomeSpentText: {
+    color: COLOR.VOILET,
+    fontSize: STYLE_CONSTANTS.font.SIZE.HUGE,
+    lineHeight: STYLE_CONSTANTS.font.LINEHEIGHT.HUGER,
+  },
+  availableBalanceText: {
+    color: COLOR.VOILET,
+    fontSize: STYLE_CONSTANTS.font.SIZE.SMALL,
+    lineHeight: STYLE_CONSTANTS.font.LINEHEIGHT.HUGISH,
+    opacity: 0.3,
+    paddingTop: STYLE_CONSTANTS.padding.HUGE,
+    textAlign: 'center',
+  },
+  availableAmountText: {
+    color: COLOR.VOILET,
+    fontSize: STYLE_CONSTANTS.font.SIZE.HUGEST,
+    /*
+  TODO : lineHeight as per XD is less than fontSize, hence cuts upper text, TBD
+  */
+    textAlign: 'center',
+  },
+  buttonTitle: {
+    color: COLOR.WHITE,
+    fontSize: STYLE_CONSTANTS.font.SIZE.LARGE,
+    lineHeight: STYLE_CONSTANTS.font.LINEHEIGHT.HUGE,
+    paddingVertical: STYLE_CONSTANTS.padding.NORMAL,
+    fontWeight: STYLE_CONSTANTS.font.WEIGHT.SEMI_BOLD,
+  },
+  button: {
+    backgroundColor: COLOR.PRIMARY,
+    borderRadius: 40,
+    marginBottom: STYLE_CONSTANTS.margin.HUMONGOUS,
+    marginTop: STYLE_CONSTANTS.margin.HUGISH,
+    marginHorizontal: STYLE_CONSTANTS.margin.SMALLISH,
+  },
+  myProgressContainer: {
+    marginLeft: STYLE_CONSTANTS.margin.HUGISH,
+    marginTop: STYLE_CONSTANTS.margin.HUMONGOUS,
+    marginBottom: STYLE_CONSTANTS.margin.LARGEST,
+  },
+  myProgressText: {
+    color: COLOR.VOILET,
+    fontSize: STYLE_CONSTANTS.font.SIZE.HUGE,
+    lineHeight: STYLE_CONSTANTS.font.LINEHEIGHT.HUGEST,
+  },
+  projectedText: {
+    color: COLOR.LIGHT_VOILET,
+    fontSize: STYLE_CONSTANTS.font.SIZE.SMALL,
+    lineHeight: STYLE_CONSTANTS.font.LINEHEIGHT.HUGE,
+  },
+  monthsLeftText: {color: COLOR.VOILET},
 });
