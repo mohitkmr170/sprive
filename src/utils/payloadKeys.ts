42856929e0628a86ac0ec85890ac8d06f66b72ab
Mohit Kumar <mohitk@geekyants.com>
2019-11-21 21:17:03 +0530

    feat(payloadKeys) : payloadKeys added to utils, integrated with setOverpayment api

---
+++ src/utils/payloadKeys.ts
@@ -0,0 +1,14 @@
+/**
+ * Payload Keys for all API payloads
+ */
+
+/*
+ TODO : Need to be added throughOut the app in a separate branch
+ */
+
+export const PAYLOAD_KEYS: any = {
+  USER_ID: 'user_id',
+  OVERPAYMENT: {
+    OVERPAYMENT_AMOUNT: 'amount',
+  },
+};
