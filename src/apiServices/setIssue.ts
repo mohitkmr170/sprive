93c9daac4c418f0c9aa63dcf8e962768f28ce6a5
Mohit Kumar <mohitk@geekyants.com>
2019-11-27 18:58:46 +0530

    feat(issueScreen) : api services added

---
+++ src/apiServices/setIssue.ts
@@ -0,0 +1,13 @@
+import apiConstants from '../../config/apiConstants';
+import {postRequest} from './requestServices';
+
+const endPoint = apiConstants.API_END_POINTS.ISSUES;
+
+/**
+ * Set Mortgage data to corresponding user
+ * @param body : object : payload for POST api call
+ */
+
+export function setIssue(body: object) {
+  return postRequest(endPoint, body);
+}
