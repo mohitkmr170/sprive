f34daabb62f81782b6df59637c41a00aea81eacb
Mohit Kumar <mohitk@geekyants.com>
2019-12-03 17:23:32 +0530

    fix(saveInterest) : minor changes

--- src/screens/saveInterest/index.tsx
+++ src/screens/saveInterest/index.tsx
@@ -64,58 +64,58 @@ class UnconnectedSaveInterest extends React.Component<props, state> {
     this.props.navigation.goBack();
   };
 
-  handleSaveWithSprive = () => {
-    const {getUserInfo, reducerResponse} = this.props;
-    getAuthToken()
-      .then(async res => {
-        console.log('handleSaveWithSprive:: getAuthToken::  res-->', res);
-        if(!res){
-          // CASE-: Token Error, Token Not found
-          // HANDLE-: Route to SignUp Screen
-          this.props.navigation.navigate(NAVIGATION_SCREEN_NAME.SIGNUP_SCREEN);
-          return;
-        }
-        const {getUserInfoResponse, setUserMortgage} = this.props;
-        if (_get(getUserInfoResponse, DB_KEYS.AUTH_STATUS, false)) {
-          //set Mortgage
-          const mortgageData = {
-            mortgage_balance: _get(
-              reducerResponse,
-              DB_KEYS.FORM_MORTGAGE_MORTGAGE_AMOUNT,
-              '',
-            ).replace(/,/g, ''),
-            mortgage_term: _get(
-              reducerResponse,
-              DB_KEYS.FORM_MORTGAGE_TIMEPERIOD,
-              '',
-            ).replace(/,/g, ''),
-            mortgage_payment: _get(
-              reducerResponse,
-              DB_KEYS.FORM_MORTGAGE_MONTHLY_MORTGAGE_AMOUNT,
-              '',
-            ).replace(/,/g, ''),
-            user_id: _get(getUserInfoResponse, DB_KEYS.USER_ID, null),
-          };
-          await setUserMortgage(mortgageData);
-          const {setUserMortgageResponse} = this.props;
-          if (_get(setUserMortgageResponse, DB_KEYS.RESPONSE_DATA, null)) {
-            // CASE-: Token Sucess, Set-User-Mortgage Success
-            // HANDLE-: Route to Set Goal Screen
-            this.props.navigation.navigate(
-              NAVIGATION_SCREEN_NAME.SET_GOAL_SCREEN,
-            );
-          }
-        } else{
-          // CASE-: Token Error, Invalid or expired
-          // HANDLE-: Route to Login Screen
-          this.props.navigation.navigate(NAVIGATION_SCREEN_NAME.LOGIN_SCREEN);
-        }
-      })
-      .catch(err => {
-        // CASE-: Token Error, KeyChain Corrupted or couldn't be fetched
-        // HANDLE-: Route to SignUp Screen
-        this.props.navigation.navigate(NAVIGATION_SCREEN_NAME.SIGNUP_SCREEN);
-      });
+  handleSaveWithSprive = async () => {
+    // const {getUserInfo, reducerResponse} = this.props;
+    // getAuthToken()
+    //   .then(async res => {
+    //     console.log('handleSaveWithSprive:: getAuthToken::  res-->', res);
+    //     if(!res){
+    //       // CASE-: Token Error, Token Not found
+    //       // HANDLE-: Route to SignUp Screen
+    //       this.props.navigation.navigate(NAVIGATION_SCREEN_NAME.SIGNUP_SCREEN);
+    //       return;
+    //     }
+    //     const {getUserInfoResponse, setUserMortgage} = this.props;
+    //     if (_get(getUserInfoResponse, DB_KEYS.AUTH_STATUS, false)) {
+    //       //set Mortgage
+    //       const mortgageData = {
+    //         mortgage_balance: _get(
+    //           reducerResponse,
+    //           DB_KEYS.FORM_MORTGAGE_MORTGAGE_AMOUNT,
+    //           '',
+    //         ).replace(/,/g, ''),
+    //         mortgage_term: _get(
+    //           reducerResponse,
+    //           DB_KEYS.FORM_MORTGAGE_TIMEPERIOD,
+    //           '',
+    //         ).replace(/,/g, ''),
+    //         mortgage_payment: _get(
+    //           reducerResponse,
+    //           DB_KEYS.FORM_MORTGAGE_MONTHLY_MORTGAGE_AMOUNT,
+    //           '',
+    //         ).replace(/,/g, ''),
+    //         user_id: _get(getUserInfoResponse, DB_KEYS.USER_ID, null),
+    //       };
+    //       await setUserMortgage(mortgageData);
+    //       const {setUserMortgageResponse} = this.props;
+    //       if (_get(setUserMortgageResponse, DB_KEYS.RESPONSE_DATA, null)) {
+    //         // CASE-: Token Sucess, Set-User-Mortgage Success
+    //         // HANDLE-: Route to Set Goal Screen
+    //         this.props.navigation.navigate(
+    //           NAVIGATION_SCREEN_NAME.SET_GOAL_SCREEN,
+    //         );
+    //       }
+    //     } else{
+    //       // CASE-: Token Error, Invalid or expired
+    //       // HANDLE-: Route to Login Screen
+    //       this.props.navigation.navigate(NAVIGATION_SCREEN_NAME.LOGIN_SCREEN);
+    //     }
+    //   })
+    //   .catch(err => {
+    //     // CASE-: Token Error, KeyChain Corrupted or couldn't be fetched
+    //     // HANDLE-: Route to SignUp Screen
+    //   });
+    this.props.navigation.navigate(NAVIGATION_SCREEN_NAME.SIGNUP_SCREEN);
   };
 
   returnItem = (item: object) => {
