925f33a3c90ecccf0b97b8e1107fc3a5f861c47d
Mohit Kumar <mohitk@geekyants.com>
2019-12-26 16:42:25 +0530

    feat(sideBar) : sizebar UI, styling and navigations

--- src/screens/sideBar/index.tsx
+++ src/screens/sideBar/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ScrollView, Text, TouchableOpacity, View} from 'react-native';
+import {View, Text, TouchableOpacity, Image} from 'react-native';
 import {styles} from '../sideBar/styles';
 import {connect} from 'react-redux';
 import {GeneralStatusBar} from '../../components';
@@ -7,11 +7,25 @@ import {
   NAVIGATION_SCREEN_NAME,
   APP_CONSTANTS,
   DB_KEYS,
+  LOCALE_STRING,
+  STYLE_CONSTANTS,
 } from '../../utils/constants';
+import {
+  iNotification,
+  iAvatar,
+  iPound,
+  iSettings,
+  iSupport,
+  iUpdate,
+  iRight,
+  iLogOut,
+} from '../../assets';
+import {closeDrawer} from '../../navigation/navigationService';
 import {setAuthToken, showSnackBar} from '../../utils/helperFunctions';
+import {localeString} from '../../utils/i18n';
 import {get as _get} from 'lodash';
 import Icon from 'react-native-vector-icons/AntDesign';
-import {COLOR} from '../../utils/colors';
+import {scale, verticalScale, moderateScale} from 'react-native-size-matters';
 
 interface props {
   navigation: {
@@ -21,6 +35,9 @@ interface props {
   getUserInfoResponse: object;
 }
 
+const LIGHT_OPACITY_BLACK = 'rgba(0, 0, 0, 0.3)';
+const CLOSE_ICON_NAME = 'close';
+const CURRENT_USER_LOCATION = 'Bangalore, India';
 interface state {}
 
 export class UnconnectedSideBar extends React.Component<props, state> {
@@ -30,23 +47,50 @@ export class UnconnectedSideBar extends React.Component<props, state> {
   }
   SIDEBAR_DATA = [
     {
-      title: 'Log Out',
-      icon: 'logout',
-      action: () => this.handleLogOut(),
+      title: localeString(LOCALE_STRING.SIDE_BAR.NOTIFICATION),
+      icon: iNotification,
+      action: () => {},
+      isDisabled: false,
     },
     {
-      title: 'Overpayment History',
-      icon: 'copy1',
+      title: localeString(LOCALE_STRING.SIDE_BAR.OVER_PAYMENT_HISTORY),
+      icon: iPound,
       action: () =>
         this.props.navigation.navigate(
           NAVIGATION_SCREEN_NAME.OVERPAYMENT_HISTORY,
         ),
+      isDisabled: false,
     },
     {
-      title: 'Update Mortgage',
-      icon: 'antdesign',
+      title: localeString(LOCALE_STRING.SIDE_BAR.UPDATE_PASSWORD),
+      icon: iUpdate,
+      action: () => {},
+      isDisabled: false,
+    },
+    {
+      title: localeString(LOCALE_STRING.UPDATE_MORTGAGE.UPDATE_MORTGAGE),
+      icon: iUpdate,
       action: () =>
         this.props.navigation.navigate(NAVIGATION_SCREEN_NAME.UPDATE_MORTGAGE),
+      isDisabled: false,
+    },
+    {
+      title: localeString(LOCALE_STRING.SIDE_BAR.SETTINGS),
+      icon: iSettings,
+      action: () => {},
+      isDisabled: true,
+    },
+    {
+      title: localeString(LOCALE_STRING.SIDE_BAR.SUPPORT),
+      icon: iSupport,
+      action: () => {},
+      isDisabled: true,
+    },
+    {
+      title: localeString(LOCALE_STRING.SIDE_BAR.INVITE_FRIENDS),
+      icon: iAvatar,
+      action: () => {},
+      isDisabled: true,
     },
   ];
   handleLogOut = () => {
@@ -64,31 +108,69 @@ export class UnconnectedSideBar extends React.Component<props, state> {
       });
   };
   render() {
+    console.log('Scalling factor =>', 200, moderateScale(200));
+    const {getUserInfoResponse} = this.props;
+    let currentUserMain = _get(
+      getUserInfoResponse,
+      DB_KEYS.CURRENT_USER_EMAIL,
+      '',
+    );
     return (
-      <ScrollView contentContainerStyle={styles.mainContainer}>
-        <GeneralStatusBar />
-        {this.SIDEBAR_DATA.map((item, index) => {
-          return (
-            <TouchableOpacity
-              onPress={item.action}
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                marginTop: 8,
-              }}>
-              <Icon name={item.icon} size={20} />
-              <Text
-                key={index}
-                style={{
-                  paddingLeft: 12,
-                  fontSize: 18,
-                }}>
-                {item.title}
+      <View style={styles.mainContainer}>
+        <View style={styles.innerMainContainer}>
+          <View style={styles.centerContainer}>
+            <View style={styles.profileContainer}>
+              <Text style={styles.userNameText}>{currentUserMain}</Text>
+              <Text style={styles.userLocationText}>
+                {CURRENT_USER_LOCATION}
               </Text>
-            </TouchableOpacity>
-          );
-        })}
-      </ScrollView>
+            </View>
+            <View style={styles.centerContainer}>
+              {this.SIDEBAR_DATA.map((item, index) => {
+                return (
+                  <TouchableOpacity
+                    disabled={item.isDisabled}
+                    onPress={item.action}
+                    style={styles.sideBarDataListContainer}>
+                    <View style={styles.iconTextContainer}>
+                      <Image
+                        source={item.icon}
+                        height={STYLE_CONSTANTS.padding.LARGEST}
+                        width={STYLE_CONSTANTS.padding.LARGEST}
+                      />
+                      <Text key={index} style={styles.titleText}>
+                        {item.title}
+                      </Text>
+                    </View>
+                    <Image source={iRight} />
+                  </TouchableOpacity>
+                );
+              })}
+            </View>
+            <View>
+              <View style={styles.logOutContainer}>
+                <TouchableOpacity
+                  style={styles.logOutTouch}
+                  onPress={() => this.handleLogOut()}>
+                  <Image source={iLogOut} />
+                  <Text style={styles.logOutText}>
+                    {localeString(LOCALE_STRING.SIDE_BAR.LOG_OUT)}
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  hitSlop={APP_CONSTANTS.HIT_SLOP}
+                  onPress={() => closeDrawer()}>
+                  <Icon
+                    name={CLOSE_ICON_NAME}
+                    size={STYLE_CONSTANTS.padding.LARGEST}
+                    color={LIGHT_OPACITY_BLACK}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
     );
   }
 }
--- src/screens/sideBar/styles.tsx
+++ src/screens/sideBar/styles.tsx
@@ -1,9 +1,76 @@
 import {StyleSheet} from 'react-native';
-import {getStatusBarHeight} from 'react-native-status-bar-height';
+import {COLOR} from '../../utils/colors';
+import {STYLE_CONSTANTS} from '../../utils/constants';
 
 export const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
-    margin: 24,
+    backgroundColor: COLOR.TRANSPARENT_BLACK,
+  },
+  innerMainContainer: {
+    flex: 1,
+    paddingVertical: STYLE_CONSTANTS.margin.LARGEST,
+    borderBottomLeftRadius: STYLE_CONSTANTS.padding.SMALL,
+    borderTopLeftRadius: STYLE_CONSTANTS.padding.SMALL,
+    backgroundColor: COLOR.WHITE,
+  },
+  centerContainer: {flex: 1},
+  profileContainer: {
+    paddingVertical:
+      STYLE_CONSTANTS.padding.HUGE + STYLE_CONSTANTS.padding.NORMAL,
+    paddingRight: 2 * STYLE_CONSTANTS.padding.HUGE,
+    paddingLeft:
+      STYLE_CONSTANTS.padding.HUGE + STYLE_CONSTANTS.padding.SMALLEST,
+    borderBottomColor: COLOR.BLACK_OPACITY_TEN,
+    borderBottomWidth: 1,
+  },
+  userNameText: {
+    fontSize: STYLE_CONSTANTS.font.SIZE.HUGE,
+    lineHeight: STYLE_CONSTANTS.font.LINEHEIGHT.HUGE,
+    color: COLOR.VOILET,
+    fontWeight: STYLE_CONSTANTS.font.WEIGHT.SEMI_BOLD,
+  },
+  userLocationText: {
+    fontSize: STYLE_CONSTANTS.font.SIZE.SMALL,
+    lineHeight: STYLE_CONSTANTS.font.LINEHEIGHT.LARGISH,
+    color: COLOR.VOILET,
+  },
+  sideBarDataListContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: STYLE_CONSTANTS.padding.LARGEST,
+    paddingLeft:
+      STYLE_CONSTANTS.padding.HUGE + STYLE_CONSTANTS.padding.SMALLEST,
+    paddingRight: STYLE_CONSTANTS.padding.HUGE,
+    borderBottomColor: COLOR.BLACK_OPACITY_TEN,
+    borderBottomWidth: 1,
+    justifyContent: 'space-between',
+  },
+  iconTextContainer: {flexDirection: 'row'},
+  titleText: {
+    paddingLeft: STYLE_CONSTANTS.padding.SMALL,
+    fontSize: STYLE_CONSTANTS.font.SIZE.NORMAL,
+    lineHeight: STYLE_CONSTANTS.font.LINEHEIGHT.LARGISH,
+    color: COLOR.BLACK,
+    opacity: 0.7,
+    fontWeight: STYLE_CONSTANTS.font.WEIGHT.SEMI_BOLD,
+  },
+  logOutContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: STYLE_CONSTANTS.padding.LARGEST,
+    borderTopColor: COLOR.BLACK_OPACITY_TEN,
+    borderTopWidth: 1,
+    paddingLeft:
+      STYLE_CONSTANTS.padding.HUGE + STYLE_CONSTANTS.padding.SMALLEST,
+    paddingRight: STYLE_CONSTANTS.padding.HUGE,
+  },
+  logOutTouch: {flexDirection: 'row', flex: 1},
+  logOutText: {
+    color: COLOR.BLACK,
+    opacity: 0.3,
+    fontSize: STYLE_CONSTANTS.font.SIZE.NORMAL,
+    lineHeight: STYLE_CONSTANTS.font.LINEHEIGHT.LARGISH,
+    paddingLeft: STYLE_CONSTANTS.padding.SMALL,
   },
 });
