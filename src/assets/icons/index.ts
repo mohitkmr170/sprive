312db67d71fbc59d02bb1f78eadeac2d8f29be12
Mohit Kumar <mohitk@geekyants.com>
2019-12-30 14:22:49 +0530

    feat(icons, images) : splashScreen background and sprive icon added to assets

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,4 @@
 export const iBack = require('../icons/arrow-left.png');
 export const iEmail = require('../icons/at_the_rate.png');
 export const iLock = require('../icons/padlock.png');
+export const iSprive = require('../icons/Sprive-Full-Mark.png');
