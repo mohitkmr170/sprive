c2294f3fdaac9afffbdad21729441c96a89a4cca
Mohit Kumar <mohitk@geekyants.com>
2020-01-21 21:00:38 +0530

    feat(IOS) : oneSignal push notification setup for IOS

--- ios/Sprive/AppDelegate.m
+++ ios/Sprive/AppDelegate.m
@@ -14,9 +14,13 @@
 #import <React/RCTLinkingManager.h>
 #import "RNSplashScreen.h"
 @implementation AppDelegate
+@synthesize oneSignal = _oneSignal;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  NSURL *jsCodeLocation;
+  self.oneSignal = [[RCTOneSignal alloc] initWithLaunchOptions:launchOptions
+                                                       appId:@"ce763fbb-0f60-4f44-b709-30eedbf62388"];
   if ([FIRApp defaultApp] == nil) {
       [FIRApp configure];
   }
