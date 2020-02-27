69a0f5dba9df45b34c4123b7a41b00236efb0431
Mohit Kumar <mohitk@geekyants.com>
2020-02-28 01:20:24 +0530

    feat(store) : getUserAddress reducer added to store

---
+++ src/store/reducers/pendingTask/getUserAddress.ts
@@ -0,0 +1,23 @@
+import {getUserAddress as getApi} from '../../../apiServices';
+import {StoreFetchableData} from '../base';
+import {showSnackBar} from '../../../utils';
+
+class getUserAddressData extends StoreFetchableData {
+  constructor() {
+    super('getUserAddress', getApi);
+  }
+  fetchCall(data: any, moreData: any) {
+    return dispatch =>
+      dispatch(this.actions.fetch()) &&
+      this.fetchData(data, moreData)
+        .then((res: any) => {
+          dispatch(this.actions.response(res));
+        })
+        .catch((err: any) => {
+          dispatch(this.actions.error(err));
+          showSnackBar(err);
+        });
+  }
+}
+
+export var getUserAddress = new getUserAddressData();
--- src/store/reducers/pendingTask/index.tsx
+++ src/store/reducers/pendingTask/index.tsx
@@ -1,2 +1,3 @@
 export * from './getPendingTask';
 export * from './taskHandler';
+export * from './getUserAddress';
