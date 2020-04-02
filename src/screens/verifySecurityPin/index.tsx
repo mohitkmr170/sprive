3dabc8bb7ceb343856186e4f6ce1430146c9e01c
Mohit Kumar <mohitk@geekyants.com>
2020-04-02 18:03:08 +0530

    feat(verifySecurityPin) : Platform and Biometric type condition added before setting pin

--- src/screens/verifySecurityPin/index.tsx
+++ src/screens/verifySecurityPin/index.tsx
@@ -6,6 +6,7 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
   Alert,
+  Platform,
 } from 'react-native';
 import {
   Header,
@@ -31,6 +32,7 @@ import {
   setUserAuthPin,
 } from '../../store/reducers';
 import {get as _get} from 'lodash';
+import FingerprintScanner from 'react-native-fingerprint-scanner';
 
 interface props {
   navigation: {
@@ -43,9 +45,11 @@ interface props {
   getUserInfo: () => void;
   setUserAuthPin: (payload: object) => void;
   setUserAuthPinResponse: object;
+  handlePopupDismissed: () => void;
 }
 interface state {
   verifyCodeValue: string;
+  loading: boolean;
 }
 
 export class UnconnectedVerifySecurityPin extends React.Component<
@@ -56,9 +60,19 @@ export class UnconnectedVerifySecurityPin extends React.Component<
     super(props);
     this.state = {
       verifyCodeValue: '',
+      loading: false,
     };
     StatusBar.setBackgroundColor(COLOR.WHITE, true);
   }
+
+  setFaceId = async () => {
+    const payload = {
+      [PAYLOAD_KEYS.TWO_FACTOR_AUTH.IS_FACE_ID_ENABLED]: true,
+    };
+    await this.props.updateUserProfile(payload, {
+      id: _get(this.props.getUserInfoResponse, DB_KEYS.DATA_ID, null),
+    });
+  };
 
   handleCode = (code: string) => {
     const {updateUserProfile, getUserInfoResponse} = this.props;
@@ -68,27 +82,47 @@ export class UnconnectedVerifySecurityPin extends React.Component<
     if (code.length === 5) {
       if (prevCode === code) {
         this.setState({verifyCodeValue: code}, () => {});
-        Alert.alert(
-          '',
-          localeString(
-            LOCALE_STRING.SECURE_LOGIN.DO_YOU_WANT_TO_ENABLE_FACE_ID,
-          ),
-          [
-            {
-              text: localeString(LOCALE_STRING.GLOBAL.NO),
-              onPress: () => {
+        if (Platform.OS === 'ios') {
+          FingerprintScanner.isSensorAvailable()
+            .then(biometrictype => {
+              console.log('biometric support', biometrictype);
+              if (biometrictype === 'Face ID') {
+                console.log('biometric type Face ID enrolled', biometrictype);
+                Alert.alert(
+                  '',
+                  localeString(
+                    LOCALE_STRING.SECURE_LOGIN.DO_YOU_WANT_TO_ENABLE_FACE_ID,
+                  ),
+                  [
+                    {
+                      text: localeString(LOCALE_STRING.GLOBAL.NO),
+                      onPress: () => {
+                        this.setSecurePin(code);
+                      },
+                    },
+                    {
+                      text: localeString(LOCALE_STRING.GLOBAL.YES),
+                      onPress: async () => {
+                        await this.props.getUserInfo();
+                        this.setFaceId();
+                        this.setSecurePin(code);
+                      },
+                    },
+                  ],
+                );
+              } else {
+                console.log(
+                  'biometric type Face ID not enrolled',
+                  biometrictype,
+                );
                 this.setSecurePin(code);
-              },
-            },
-            {
-              text: localeString(LOCALE_STRING.GLOBAL.YES),
-              onPress: async () => {
-                await this.props.getUserInfo();
-                this.setSecurePin(code);
-              },
-            },
-          ],
-        );
+              }
+            })
+            .catch(error => {
+              console.log('biometrictype not supported ::', error);
+              this.setSecurePin(code);
+            });
+        } else this.setSecurePin(code);
       } else {
         showSnackBar(
           {},
@@ -99,6 +133,7 @@ export class UnconnectedVerifySecurityPin extends React.Component<
   };
 
   setSecurePin = async (pin: string) => {
+    this.setState({loading: true});
     const {setUserAuthPin, getUserInfoResponse} = this.props;
     const payload = {
       [PAYLOAD_KEYS.USER_ID]: _get(getUserInfoResponse, DB_KEYS.USER_ID, null),
@@ -106,7 +141,16 @@ export class UnconnectedVerifySecurityPin extends React.Component<
     };
     await setUserAuthPin(payload);
     const {setUserAuthPinResponse} = this.props;
+    const payloadUpdateProfile = {
+      [PAYLOAD_KEYS.TWO_FACTOR_AUTH.IS_PIN_ENABLED]: true,
+    };
+    await this.props.updateUserProfile(payloadUpdateProfile, {
+      id: _get(this.props.getUserInfoResponse, DB_KEYS.DATA_ID, null),
+    });
+    await this.props.getUserInfo();
     if (!_get(setUserAuthPinResponse, DB_KEYS.ERROR, true)) {
+      Keyboard.dismiss();
+      this.setState({loading: false});
       showSnackBar(
         {},
         _get(setUserAuthPinResponse, DB_KEYS.RESPONSE_MESSAGE, ''),
@@ -117,10 +161,8 @@ export class UnconnectedVerifySecurityPin extends React.Component<
 
   render() {
     const {getUserInfoResponse, setUserAuthPin} = this.props;
-    if (
-      _get(getUserInfoResponse, DB_KEYS.IS_FETCHING, false) ||
-      _get(setUserAuthPin, DB_KEYS.IS_FETCHING, false)
-    ) {
+    const {loading} = this.state;
+    if (loading) {
       return <LoadingModal loadingText="Loading..." />;
     } else
       return (
